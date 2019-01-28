506bdc2faf72741ddf2fd9f3790ed7b0739a4cf0
NJIMAMTO <s17018@g.metro-cit.ac.jp>
2019-01-28 12:42:01 +0900

    add

--- image_to_video.py
+++ image_to_video.py
@@ -4,17 +4,17 @@ import cv2
 path = "/media/mokugyo/ボリューム/3Dface"
 folder = ["F_Angry","F_Disgust","F_Fear","F_Happy","F_Neutral","F_Surprise","F_Unhappy",
         "M_Angry","M_Disgust","M_Fear","M_Happy","M_Neutral","M_Surprise","M_Unhappy"]
-V_S = ["V0S","V2L"]
-E_S = ["_A","_D","_F","_H","_N","_S","_U"] 
+V_S = ["V0S","V2L","V4L"]
+#E_S = ["_A","_D","_F","_H","_N","_S","_U"] 
 
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
 
 for i in range(0,14):
-        for x in range(0, 2):
+        for x in range(0, 3):
                 #対象文字列(*V_S*)を含むファイルを検索し一覧を取得
-                files = glob.glob(path + "/" + folder[i] + "/*"+ V_S[x] + E_S[i%7] +"*.JPG")
+                files = glob.glob(path + "/" + folder[i] + "/*"+ V_S[x] + "*.JPG")
                 #videoを作成
-                video = cv2.VideoWriter( path + "/" + folder[i] + "/" + V_S[x] + E_S[i%7] + '.mp4', fourcc, 30.0, (1200, 1200))
+                video = cv2.VideoWriter( path + "/" + folder[i] + "/" + V_S[x] + '.mp4', fourcc, 3.0, (1200, 1200))
     
                 length = len(files)
                 #取得したファイル名から画像を読み込み動画化する
